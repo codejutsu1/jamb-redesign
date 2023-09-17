21dc3f810b4d88a397219a320257f7b7e1f616e9
codejutsu1 <codejutsu@protonmail.com>
2023-09-17 10:45:46 +0100

    added the slideshow

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'nunito' : ['nunito', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
